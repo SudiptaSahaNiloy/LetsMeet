e0bb43ea77942d9c346b75c275dfa602266d5432
Sudipta Saha Niloy <niloy305968@gmail.com>
2021-03-24 18:05:15 +0600

    Day 2.1

--- app/src/main/java/com/example/letsmeet/Login_Screen.java
+++ app/src/main/java/com/example/letsmeet/Login_Screen.java
@@ -2,7 +2,9 @@ package com.example.letsmeet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login_Screen extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Login_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login__screen);
+    }
+
+    public void MainActivity(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
