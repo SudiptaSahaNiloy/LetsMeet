22b7e5d4c6d63873006ce97597aafec9c6d9500e
Sudipta Saha Niloy <niloy305968@gmail.com>
2021-05-01 18:56:40 +0600

    Day 8

---
+++ app/src/main/java/com/example/letsmeet/Register_Screen.java
@@ -0,0 +1,14 @@
+package com.example.letsmeet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register__screen);
+    }
+}
